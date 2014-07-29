57d9085060385ea2d11b550965cea4f6cba88302
rpietzsch <rpietzsch@gmail.com>
2014-07-29 19:37:45 +0200

    changed to watchdog

--- trx_parse/trx_parser.py
+++ trx_parse/trx_parser.py
@@ -5,6 +5,7 @@ import httplib, socket
 
 from os import listdir
 from os.path import getmtime
+from collections import deque
 
 # [sudo] pip install python-dateutil
 from dateutil import parser 
@@ -23,13 +24,13 @@ global rsine_host, rsine_port
 rsine_host = "192.168.33.1"
 rsine_port = "2221"
 
-global debug, total_nr_triples, trx_parsed_files, from_beginning, now, sleep
+global debug, total_nr_triples, trx_parsed_files, from_beginning, sleep, trx_files_to_parse, last_ex
 debug = False
-from_beginning = False
 sleep = 1
-now = time.time()
 total_nr_triples = 0
+trx_files_to_parse = deque()
 trx_parsed_files = [] # [{String path, int offset, float cmtime}, float mtime ...]
+last_ex = False
 
 
 def getFilesForPattern(pattern = virt_log_path + "*.trx"):
@@ -68,6 +69,7 @@ def gtlt(string):
 
 # do a rest call to rsine 
 def placeRestCall(op, s, p, o):
+	global last_ex
 	if op == "I":
 		operation = "add"
 	elif op == "D":
@@ -78,7 +80,6 @@ def placeRestCall(op, s, p, o):
 			   "Accept": "text/plain"}
 
 	conn = httplib.HTTPConnection(rsine_host, rsine_port)
-
 	try:
 		conn.request("POST", "", body, headers)
 		res = conn.getresponse()
@@ -88,18 +89,24 @@ def placeRestCall(op, s, p, o):
 			print body
 			print res.status, res.reason	
 			return False
+		last_ex = False
 	except socket.error as ex:
-		print ex
-		print "Error connecting to rsine service."
+		if not last_ex or last_ex.errno != ex.errno:
+			print ex
+			print "Error connecting to rsine service."
+		last_ex = ex
 	except httplib.BadStatusLine as ex:
-		print ex
-		print "Lost connection to rsine service."
+		if not last_ex or last_ex.errno != ex.errno:
+			print ex
+			print "Lost connection to rsine service."
+		last_ex=ex
 	return False
 # end placeRestCall
 
 
 # parses <file> beginning at position <offset>, return last <offset> 
 def parse(file, offset):
+	global total_nr_triples
 	new_offset = 0
 	offset = int(float(offset))
 	cmdline = virt_path + "/bin/isql 1111 dba dba \"EXEC=elds_read_trx('" + file + "', " + `offset` + "); exit;\""
@@ -128,87 +135,71 @@ def parse(file, offset):
 	
 	if triple_count > 0:
 		print "# of send triples: " + `triple_count` + "; all ok: " + `ok`
+		if ok:
+			total_nr_triples += triple_count
 	if ok:
 		return new_offset
 	else: 
 		return 0
 # end parse
 
+
+def handleFile(path):
+	trx_parsed_file = search("path", path, trx_parsed_files)
+
+	mtime = os.path.getmtime(path)
+	cmtime = time.ctime(mtime)
+	
+	if len(trx_parsed_file) > 0: 
+		print "parsing, starting at " + `trx_parsed_file[0]['offset']` + ", " + path
+		new_offset = parse(path, trx_parsed_file[0]['offset'])
+		if new_offset != trx_parsed_file[0]['offset']:
+			trx_parsed_file[0]['offset'] = new_offset
+			print "new_offset: " + `new_offset`
+	else:
+		print "parsing (new file), starting at 0, " + path 
+		offset = 0
+		new_offset = parse(path, offset)
+		trx_parsed_files.append({'path': path, 'offset': new_offset, 'cmtime': cmtime})
+		if new_offset != 0:
+			print "new_offset: " + `new_offset`
+#end handleFile
+
+
 class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*.trx"]
+	patterns = ["*.trx"]
 
-    def process(self, event):
-        """
-        event.event_type 
-            'modified' | 'created' | 'moved' | 'deleted'
-        event.is_directory
-            True | False
-        event.src_path
-            virt_log_path
-        """
-        # the file will be processed there
-        print event.src_path, event.event_type  # print now only for degug
+	def process(self, event):
+		"""
+		event.event_type 
+		    'modified' | 'created' | 'moved' | 'deleted'
+		event.is_directory
+		    True | False
+		event.src_path
+		    virt_log_path
+		"""
+		# the file will be processed there
+		# print event.src_path, event.event_type  # print now only for degug
+		if not event.src_path in trx_files_to_parse:
+			trx_files_to_parse.append(event.src_path)
+		# print trx_files_to_parse
 
-        
+	def on_modified(self, event):
+		self.process(event)
 
-    def on_modified(self, event):
-        self.process(event)
-
-    def on_created(self, event):
-        self.process(event)
+	def on_created(self, event):
+		self.process(event)
 
 observer = Observer()
 observer.schedule(MyHandler(), virt_log_path)
 observer.start()
 try:
 	while True:
+		if trx_files_to_parse:
+			last_ex = False
+			handleFile(trx_files_to_parse.popleft())
 		time.sleep(sleep)
 except KeyboardInterrupt:
 	observer.stop()
+	print "total # of triples: " + `total_nr_triples`
 observer.join()
-
-# Main loop
-loop = 0
-while True:
-	loop += 1
-	
-	if debug:
-		print "Loop: " + `loop`
-	
-	trx_files_list = getFilesForPattern()
-
-	trx_file_index = 0
-
-	for trx_file in trx_files_list:
-		trx_file_index += 1
-		trx_parsed_file = search("path", trx_file['path'], trx_parsed_files)
-
-		path = trx_file['path']
-		cmtime = trx_file['cmtime']
-		mtime = trx_file['mtime']
-		
-		if len(trx_parsed_file) > 0: 
-			if trx_parsed_file[0]['cmtime'] != cmtime:
-				print "parsing (cmtime differs), starting at " + `trx_parsed_file[0]['offset']` + " - " + path
-				new_offset = parse(path, trx_parsed_file[0]['offset'])
-				if new_offset != trx_parsed_file[0]['offset']:
-					trx_parsed_file[0]['offset'] = new_offset
-					print "new_offset: " + `new_offset`
-			else:
-				if debug:
-					print "skipping file (no mtime change) - " + path
-		else:
-			if not from_beginning and mtime < now:
-				print "skipping file as modified prior to start - " + path
-				trx_parsed_files.append({'path': path, 'offset': 0, 'cmtime': cmtime})
-				continue
-			
-			print "parsing (new file), starting at 0 - " + path 
-			offset = 0
-			new_offset = parse(path, offset)
-			trx_parsed_files.append({'path': path, 'offset': new_offset, 'cmtime': cmtime})
-			if new_offset != trx_parsed_file[0]['offset']:
-				print "new_offset: " + `new_offset`
-
-	print "sleeping " + `sleep` + "s"
-	time.sleep(sleep)
